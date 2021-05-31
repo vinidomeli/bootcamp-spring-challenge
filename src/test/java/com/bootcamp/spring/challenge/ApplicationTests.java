c2c5b6269cd1817b2374aa65cef8cc7d260ee26c
Vinicius Lima <vinicius.limafernandes@mercadolivre.com>
2021-05-31 18:08:28 -0300

    spring project with gradle as build tool

---
+++ src/test/java/com/bootcamp/spring/challenge/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.spring.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
