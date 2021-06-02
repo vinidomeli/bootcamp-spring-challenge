58f33880bd884d8f5030fd1aaa5359d6d946a5cd
Vinicius Lima <vinicius.limafernandes@mercadolivre.com>
2021-06-02 15:12:50 -0300

    Application runner

--- src/main/java/com/bootcamp/spring/challenge/ApplicationRunner.java
+++ src/main/java/com/bootcamp/spring/challenge/ApplicationRunner.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class ApplicationRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(final String[] args) {
+        SpringApplication.run(ApplicationRunner.class, args);
+    }
 
 }
